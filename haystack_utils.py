c6e834b869ff14eb3cf4be19fc9cd288562e5821
lovis-heindrich <heindrich.lovis@gmail.com>
2023-06-19 19:06:28 +0000

    clean mlp difference notebook

---
+++ haystack_utils.py
@@ -0,0 +1,63 @@
+from transformer_lens import HookedTransformer
+from jaxtyping import Float
+from torch import Tensor
+import einops
+from tqdm.auto import tqdm
+import torch
+
+
+def load_txt_data(path: str) -> list[str]:
+    """Loads line separated dataset examples from a text file.
+
+    Args:
+        path (str): Path to the text file.
+
+    Returns:
+        list[str]: List of examples.
+    """
+    with open(path, "r") as f:
+        data = f.read().split("\n")
+    min_len = min([len(example) for example in data])
+    max_len = max([len(example) for example in data])
+    print(
+        f"{path}: Loaded {len(data)} examples with {min_len} to {max_len} characters each."
+    )
+    return data
+
+
+def get_mlp_activations(
+    prompts: list[str],
+    layer: int,
+    model: HookedTransformer,
+    num_prompts: int = -1,
+    context_crop_start=10,
+    context_crop_end=400,
+    mean=True
+) -> Float[Tensor, "num_activations d_mlp"]:
+    """Runs the model through a list of prompts and stores the mlp activations for a given layer. Might be slow for large batches as examples are run one by one.
+
+    Args:
+        prompts (list[str]): Prompts to run through the model.
+        layer (int): Layer to extract activations from.
+        model (HookedTransformer): Model to run.
+        num_prompts (int, optional): Number of prompts to run. Defaults to -1 (all prompts).
+        context_crop_start (int, optional): Crops the start context position to avoid low neuron activations. Defaults to 10.
+        context_crop_end (int, optional): Crops the end context position to avoid low neuron activations. Defaults to 400.
+
+    Returns:
+        Float[Tensor, "num_activations d_mlp"]: Stacked activations for each prompt and position.
+    """
+    acts = []
+    act_label = f"blocks.{layer}.mlp.hook_post"
+    if num_prompts == -1:
+        num_prompts = len(prompts)
+    for i in tqdm(range(num_prompts)):
+        tokens = model.to_tokens(prompts[i])
+        _, cache = model.run_with_cache(tokens)
+        act = cache[act_label][:, context_crop_start:context_crop_end, :]
+        act = einops.rearrange(act, "batch pos d_mlp -> (batch pos) d_mlp")
+        acts.append(act)
+    acts = torch.concat(acts, dim=0)
+    if mean:
+        return torch.mean(acts, dim=0)
+    return acts
