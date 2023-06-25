760e3e708e4e0ac4fbb6d63c580a89bbe282b959
Lucia Quirke <luciarosequirke@gmail.com>
2023-06-25 04:38:53 +0000

    add plots for language specificity of layers

--- archive/diff_token_len_matched_word_fr_en.py
+++ archive/diff_token_len_matched_word_fr_en.py
@@ -11,3 +11,26 @@
 #     ("He loves her.", "Il l'aime."),
 #     ("She is going to school.", "Elle va à l'école.")
 # ]
+
+
+# API that doesn't work
+# from googletrans import Translator
+# translator = Translator()
+# english_data = haystack_utils.load_txt_data("kde4_english.txt")
+
+# print(translator.translate("hello", src='en', dest='fr'))
+
+# same_token_len_pairs_large = []
+# for item in english_data:
+#     for word in item.split(" "):
+#         try:
+#             translation = translator.translate(word, src='en', dest='fr')
+#         except:
+#             continue
+#         if translation is None or translation.text is None:
+#             continue
+
+#         if model.to_tokens(" " + word).shape[1] == model.to_tokens(" " + translation.text).shape[1]:
+#             same_token_len_pairs_large.append((word, translation.text))
+#         if len(same_token_len_pairs_large) == 100:
+#             break
