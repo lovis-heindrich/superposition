e2cb6012cdb1448e46f11e085042b2a3bf0090ed
Lucia Quirke <luciarosequirke@gmail.com>
2023-06-25 03:21:44 +0000

    language specific representations

---
+++ archive/diff_token_len_matched_word_fr_en.py
@@ -0,0 +1,13 @@
+# Different tokens lengths
+# sentence_pairs = [
+#     ("I am eating an apple.", "Je mange une pomme."),
+#     ("She reads the book.", "Elle lit le livre."),
+#     ("He is walking in the park.", "Il se promène dans le parc."),
+#     ("We are studying French.", "Nous étudions le français."),
+#     ("They are watching a movie.", "Ils regardent un film."),
+#     ("You are drinking water.", "Tu bois de l'eau."),
+#     ("The cat is sleeping on the chair.", "Le chat dort sur la chaise."),
+#     ("The children are playing in the garden.", "Les enfants jouent dans le jardin."),
+#     ("He loves her.", "Il l'aime."),
+#     ("She is going to school.", "Elle va à l'école.")
+# ]
