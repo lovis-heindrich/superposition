436613012ce088941c7b9c0562cf49993437a69d
Lucia Quirke <luciarosequirke@gmail.com>
2023-08-01 02:36:56 +0000

    Extract out color method

--- pythia_160m_utils.py
+++ pythia_160m_utils.py
@@ -3,6 +3,10 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 import sklearn.metrics
 import haystack_utils
+import matplotlib.pyplot as plt
+import re
+from IPython.display import display, HTML
+
 
 ### Sparse probing utils
 
@@ -47,3 +51,53 @@ def ablation_effect(model, data, fwd_hooks):
 
     print(original_loss, ablated_loss)
     print(f'{(ablated_loss - original_loss) / original_loss * 100:2f}% loss increase')
+
+
+def color_strings_by_value(strings: list[str], color_values: list[float], max_value: float=None, additional_measures: list[list[float]] | None = None, additional_measure_names: list[str] | None = None):
+    """
+    Magic GPT function that prints a string as HTML and colors it according to a list of color values. 
+    Color values are normalized to the max value preserving the sign.
+    """
+
+    def normalize(values, max_value=None, min_value=None):
+        if max_value is None:
+            max_value = max(values)
+        if min_value is None:
+            min_value = min(values)
+        min_value = abs(min_value)
+        normalized = [(value / max_value if value > 0 else value / min_value) for value in values]
+        return normalized
+    
+    # Normalize color values
+    normalized_values = normalize(color_values, max_value, max_value)
+
+    html = "<div>"
+    cmap = cmap=plt.cm.Pastel1
+
+    for i in range(len(strings)):
+        normalized_color = normalized_values[i].cpu()
+        
+        # Use colormap to get RGB values
+        red, green, blue, _ = cmap(normalized_color)
+
+        # Scale RGB values to 0-255
+        red, green, blue = [int(255*v) for v in (red, green, blue)]
+        
+        # Calculate luminance to determine if text should be black
+        luminance = (0.299 * red + 0.587 * green + 0.114 * blue) / 255
+        
+        # Determine text color based on background luminance
+        text_color = "black" if luminance > 0.5 else "white"
+
+        visible_string = re.sub(r'\s+', '_', strings[i])
+        
+        html += f'<span style="background-color: rgb({red}, {green}, {blue}); color: {text_color}; padding: 2px;" '
+        html += f'title="Difference: {color_values[i]:.4f}' 
+        if additional_measure_names is not None:
+            for j in range(len(additional_measure_names)):
+                html += f', {additional_measure_names[j]}: {additional_measures[j][i]:.4f}'
+        html += f'">{visible_string}</span>'
+    html += '</div>'
+
+    # Print the HTML in Jupyter Notebook
+    display(HTML(html))
