1338ed6a5619faa050b5d368bc6789eb1dfe0240
lovis-heindrich <heindrich.lovis@gmail.com>
2023-08-01 21:37:03 +0000

    loss examples

--- trimodal_L11N2014.py
+++ trimodal_L11N2014.py
@@ -11,6 +11,8 @@ from IPython.display import display, clear_output
 import pandas as pd
 import numpy as np
 import plotly.express as px 
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 from collections import defaultdict
 import matplotlib.pyplot as plt
 import re
@@ -45,23 +47,20 @@ model = HookedTransformer.from_pretrained("EleutherAI/pythia-160m",
     fold_ln=True,
     device=device)
 
-
-german_data = haystack_utils.load_json_data("data/german_europarl.json")[:200]
-english_data = haystack_utils.load_json_data("data/english_europarl.json")[:200]
-# %%
-
-LAYER, NEURON = 11, 2014
-neuron_activations = haystack_utils.get_mlp_activations(german_data, LAYER, model, neurons=torch.LongTensor([NEURON]), mean=False).flatten()
-# %%
-px.histogram(neuron_activations.cpu().numpy())
-
+with open("./data/wmt_europarl.pkl", "rb") as f:
+    data = pickle.load(f)
 #%% 
 # 2014: es, sv, de, en
 # 1819: de, nl, it, (en)
+LAYER, NEURON = 11, 2014
 languages = ["es", "sv", "de", "en"]
 
-with open("./data/wmt_europarl.pkl", "rb") as f:
-    data = pickle.load(f)
+# LAYER, NEURON = 11, 1819
+# languages = ["de", "nl", "it", "en"]
+
+# LAYER, NEURON = 8, 2994
+# languages = ["de", "nl", "it", "en", "sv"]
+
 
 #%%
 language_activations = []
@@ -71,18 +70,37 @@ for language in tqdm(languages):
     language_activations.append(language_activation.cpu().numpy())
 
 #%%
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
 
-def plot_histograms(data, names):
+
+def plot_histograms(data, names, title=""):
     fig = make_subplots()
     for arr, name in zip(data, names):
         fig.add_trace(go.Histogram(x=arr, name=name, nbinsx=500, opacity=0.5, histnorm='probability density'))
-    fig.update_layout(barmode='overlay')
+    fig.update_layout(barmode='overlay', title=title)
     fig.show()
 
-plot_histograms(language_activations, languages)
+plot_histograms(language_activations, languages, title=f'L{LAYER}N{NEURON} activations on different languages')
 
+# %%
+#mean_activation = np.mean(language_activations[languages.index("en")])
+mean_activation = np.mean(language_activations[languages.index("de")])
+print(mean_activation)
+# %%
+# print some high loss examples
+mean_ablate_hook = hook_utils.get_ablate_neuron_hook(LAYER, NEURON, float(mean_activation))
+
+prompts = [x["text"] for x in data["de"][:200]]
+
+def get_loss_diff(prompt):
+    loss = model(prompt, return_type="loss", loss_per_token=True)
+    with model.hooks(fwd_hooks=[mean_ablate_hook]):
+        ablated_loss = model(prompt, return_type="loss", loss_per_token=True)
+    return (ablated_loss - loss).flatten().tolist()
+
+for prompt in prompts[5:10]:
+    diff = get_loss_diff(prompt)
+    str_tokens = model.to_str_tokens(model.to_tokens(prompt).flatten())[1:]
+    haystack_utils.clean_print_strings_as_html(str_tokens, diff)
 
 # %%
 ranges = [(-10, 0.2), (1, 5), (9, 50)]
