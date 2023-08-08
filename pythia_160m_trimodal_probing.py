65472b5f3370a2a730b4e21000f72037d5c487c3
Lucia Quirke <luciarosequirke@gmail.com>
2023-08-08 05:05:36 +0000

    generalise probe to uses slice

--- pythia_160m_trimodal_probing.py
+++ pythia_160m_trimodal_probing.py
@@ -35,6 +35,7 @@ from hook_utils import save_activation
 import haystack_utils
 import hook_utils
 import plotting_utils
+from sklearn import preprocessing
 
 %reload_ext autoreload
 %autoreload 2
@@ -60,39 +61,37 @@ def get_next_token_punctuation_mask(tokens: torch.LongTensor) -> torch.BoolTenso
         next_token_punctuation_mask[i] = next_is_space
     return next_token_punctuation_mask
 # %%
+def get_new_word_labels(model: HookedTransformer, tokens: torch.Tensor) -> list[bool]:
+    prompt_labels = []
+    for i in range(tokens.shape[0] - 1):
+        next_token_str = model.to_single_str_token(tokens[i + 1].item())
+        next_is_space = next_token_str[0] in [" "] # [" ", ",", ".", ":", ";", "!", "?"]
+        prompt_labels.append(next_is_space)
+    return prompt_labels
 
-def get_new_word_dense_probe_f1(model, german_data, layer, neurons=torch.LongTensor([NEURON])) -> tuple[np.ndarray, np.ndarray]:
-    ''' Compute F1 of predicting word end from activation and plot performance of each peak'''
-    def get_new_word_labeled_activations():
-        '''Get activations and labels for predicting word end from activation'''
-        hook_name = f'blocks.{layer}.mlp.hook_post'
-        activations = []
-        labels = []
-        for prompt in german_data:
-            tokens = model.to_tokens(prompt)[0]
-            prompt_activations = []
-            with model.hooks([(hook_name, save_activation)]):
+def get_new_word_labels_and_activations(model, german_data, hook_name, activation_slice=np.s_[0, :-1, NEURON:NEURON+1]) -> tuple[np.ndarray, np.ndarray]:
+    '''Get activations and labels for predicting word end from activation'''
+    activations = []
+    labels = []
+    for prompt in german_data:
+        tokens = model.to_tokens(prompt)[0]
+
+        with model.hooks([(hook_name, save_activation)]):
                 model(tokens)
-            prompt_activations = model.hook_dict[hook_name].ctx['activation'][0, :-1, neurons].cpu().numpy()
-            activations.append(prompt_activations)
-            
-            
-            prompt_labels = []
-            for i in range(tokens.shape[0] - 1):
-                next_token_str = model.to_single_str_token(tokens[i + 1].item())
-                next_is_space = next_token_str[0] in [" "] # [" ", ",", ".", ":", ";", "!", "?"]
-                prompt_labels.append(next_is_space)
-            labels.extend(prompt_labels)
+        prompt_activations = model.hook_dict[hook_name].ctx['activation']
+        prompt_activations = prompt_activations[activation_slice].cpu().numpy()
+        activations.append(prompt_activations)
 
-        activations = np.concatenate(activations, axis=0)
-        labels = np.array(labels)
-        assert activations.shape[0] == labels.shape[0]
+        prompt_labels = get_new_word_labels(model, tokens)
+        labels.extend(prompt_labels)
 
-        return activations, labels
+    activations = np.concatenate(activations, axis=0)
+    labels = np.array(labels)
 
-    x, y = get_new_word_labeled_activations()
-    from sklearn import preprocessing
+    assert activations.shape[0] == labels.shape[0]
+    return activations, labels
 
+def get_new_word_dense_probe_f1(x, y):
     # z-scoring can help with convergence
     scaler = preprocessing.StandardScaler().fit(x)
     x = scaler.transform(x)
@@ -101,60 +100,19 @@ def get_new_word_dense_probe_f1(model, german_data, layer, neurons=torch.LongTen
     lr_model.fit(x[:20000], y[:20000])
     preds = lr_model.predict(x[20000:])
     score = f1_score(y[20000:], preds)
-    print(score)
+    return score
 
-# %% 
-get_new_word_dense_probe_f1(model, german_data, LAYER, torch.LongTensor([NEURON]))
+# %%
+hook_name = f'blocks.{LAYER}.mlp.hook_post'
+x, y = get_new_word_labels_and_activations(model, german_data, hook_name)
+score = get_new_word_dense_probe_f1(x, y)
+print(score)
 # 88.82 f1
-# get_new_word_dense_probe_f1(model, german_data, LAYER, torch.LongTensor([neuron for neuron in range(model.cfg.d_mlp) if neuron != NEURON]))
-# %%
+# score = get_new_word_dense_probe_f1(model, german_data, hook_name, torch.LongTensor([neuron for neuron in range(model.cfg.d_mlp) if neuron != NEURON]))
 # 88.83 f1
-# get_new_word_dense_probe_f1(model, german_data, LAYER, torch.LongTensor([neuron for neuron in range(model.cfg.d_mlp)]))
-
-
-# %%
-def plot_peak_new_word_accuracies(model, german_data, layer, neuron):
-    '''Plot % of each activation peak that corresponds to word end tokens'''
-    def trimodal_interest_measure(activations):
-        diffs = torch.zeros_like(activations) + 3
-        diffs[(activations < 0.2)] = 0
-        diffs[(activations > 0.8) & (activations < 4.1)] = 1
-        diffs[(activations > 4.8)] = 2
-        return diffs
-
-    hook_name = f'blocks.{layer}.mlp.hook_post'
-    data = []
-    for prompt in german_data:
-        tokens = model.to_tokens(prompt)[0]
-        
-        with model.hooks([(hook_name, save_activation)]):
-            model(prompt)
-        pos_wise_diff = trimodal_interest_measure(model.hook_dict[hook_name].ctx['activation'][0, :, neuron])
-        for i in range(tokens.shape[0] - 1):
-            next_token_str = model.to_single_str_token(tokens[i + 1].item())
-            next_is_space = next_token_str[0] in [" ", ",", ".", ":", ";", "!", "?"]
-            if pos_wise_diff[i] == 1:
-                data.append(["Peak 1", next_is_space])
-            elif pos_wise_diff[i] == 2:
-                data.append(["Peak 2", next_is_space])
-            elif pos_wise_diff[i] == 0:
-                data.append(["Inactive", next_is_space])
-            else:
-                assert pos_wise_diff[i] == 3
-                data.append(["Unclear Peak", next_is_space])
-
-    df = pd.DataFrame(data, columns=["Peak", "BeforeNewWord"])
-
-    fig = px.histogram(df, x="Peak", color="BeforeNewWord", barmode="group", hover_data=df.columns, width=800)
-    fig.update_layout(
-        title_text='Peak data, check if next token starts with [" ", ",", ".", ":", ";", "!", "?"]', # title of plot
-        xaxis_title_text='', # xaxis label
-        yaxis_title_text='Number of tokens', # yaxis label
-        bargap=0.2, # gap between bars of adjacent location coordinates
-        bargroupgap=0.1 # gap between bars of the same location coordinates
-    )
-    fig.show()
+# score = get_new_word_dense_probe_f1(model, german_data, hook_name, torch.LongTensor([neuron for neuron in range(model.cfg.d_mlp)]))
 # %%
 
-# %%
-plot_peak_new_word_accuracies(model, german_data, LAYER, NEURON)
+# Dense probe performance on each residual and MLP out
+# We can do each MLP out using the current method
+# Need a new method for the dimensions of the residual
