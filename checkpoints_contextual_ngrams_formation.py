b3376a4f6be537fdff92b8947295ceccfc07cf3a
Lucia Quirke <luciarosequirke@gmail.com>
2023-09-25 00:29:37 +0000

    save

--- checkpoints_contextual_ngrams_formation.py
+++ checkpoints_contextual_ngrams_formation.py
@@ -24,6 +24,60 @@ import haystack_utils
 import probing_utils
 
 
+SEED = 42
+
+def set_seeds():
+    torch.manual_seed(SEED)
+    np.random.seed(SEED)
+    random.seed(SEED)
+
+    pio.renderers.default = "notebook_connected+notebook"
+
+
+def get_model(model_name: str, checkpoint: int) -> HookedTransformer:
+    model = HookedTransformer.from_pretrained(
+        model_name,
+        checkpoint_index=checkpoint,
+        center_unembed=True,
+        center_writing_weights=True,
+        fold_ln=True,
+        device="cuda" if torch.cuda.is_available() else "cpu",
+    )
+    return model
+
+
+def preload_models(model_name: str) -> int:
+    """Preload models into cache so we can iterate over them quickly and return the model checkpoint count."""
+    i = 0
+    try:
+        with tqdm(total=None) as pbar:
+            while True:
+                get_model(model_name, i)
+                i += 1
+                pbar.update(1)
+
+    except IndexError:
+        return i
+
+
+def load_language_data() -> dict:
+    """
+    Returns: dictionary keyed by language code, containing 200 lines of each language included in the Europarl dataset.
+    """
+    lang_data = {}
+    lang_data["en"] = haystack_utils.load_json_data("data/english_europarl.json")[:200]
+
+    europarl_data_dir = Path("data/europarl/")
+    for file in os.listdir(europarl_data_dir):
+        if file.endswith(".txt"):
+            lang = file.split("_")[0]
+            lang_data[lang] = haystack_utils.load_txt_data(europarl_data_dir.joinpath(file))
+
+    for lang in lang_data.keys():
+        print(lang, len(lang_data[lang]))
+    return lang_data
+
+
 def eval_prompts(prompts, model, pos=-1):
     '''Mean loss at position in prompts'''
     loss = model(prompts, return_type="loss", loss_per_token=True)[:, pos].mean().item()
@@ -85,23 +139,51 @@ def get_common_ngrams(
     ]
     return Counter(all_ngrams).most_common(top_k)
 
+
+def analyze_contextual_ngrams(model_name, save_path):
+    set_seeds()
+    num_checkpoints = preload_models(model_name)
+    lang_data = load_language_data()
+    german_data = lang_data["de"]
+
+    temp_model = get_model(model_name, 0)
+    n_layers = temp_model.cfg.n_layers
+
+    all_ignore, _ = haystack_utils.get_weird_tokens(model, plot_norms=False)    
+
+    common_tokens = haystack_utils.get_common_tokens(
+        german_data, model, all_ignore, k=100
+    )
+    top_trigrams = get_common_ngrams(model, lang_data["de"], 3, 200)
+    random_trigram_indices = np.random.choice(
+        range(len(top_trigrams)), 20, replace=False
+    )
+    random_trigrams = ["".join(top_trigrams[i][0]) for i in random_trigram_indices]
+
+    ngram_loss_dfs = []
+    with tqdm(total=num_checkpoints * n_layers) as pbar:
+        for checkpoint in range(num_checkpoints):
+            model = get_model(checkpoint)
+            for layer in range(n_layers):
+                ngram_loss_dfs.append(
+                    get_ngram_losses(model, checkpoint, random_trigrams, common_tokens)
+                )
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "--model",
+        default="EleutherAI/pythia-70m",
+        help="Name of model from TransformerLens",
+    )
+    parser.add_argument("--output_dir", default="feature_formation")
+
+    args = parser.parse_args()
+
+    save_path = os.path.join(args.output_dir, args.model)
+    os.makedirs(save_path, exist_ok=True)
     
-all_ignore, _ = haystack_utils.get_weird_tokens(model, plot_norms=False)    
+    analyze_contextual_ngrams(args.model, Path(save_path))
 
-common_tokens = haystack_utils.get_common_tokens(
-    german_data, model, all_ignore, k=100
-)
-top_trigrams = get_common_ngrams(model, lang_data["de"], 3, 200)
-random_trigram_indices = np.random.choice(
-    range(len(top_trigrams)), 20, replace=False
-)
-random_trigrams = ["".join(top_trigrams[i][0]) for i in random_trigram_indices]
-
-ngram_loss_dfs = []
-with tqdm(total=num_checkpoints * n_layers) as pbar:
-    for checkpoint in range(num_checkpoints):
-        model = get_model(checkpoint)
-        for layer in range(n_layers):
-            ngram_loss_dfs.append(
-                get_ngram_losses(model, checkpoint, random_trigrams, common_tokens)
-            )
