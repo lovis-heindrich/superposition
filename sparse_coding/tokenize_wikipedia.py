d03307633c4ea03613585f126d0519d03d0c4da5
Lucia Quirke <luciarosequirke@gmail.com>
2023-10-19 13:13:26 +1100

    rename WIP tokenize script

--- sparse_coding/tokenize_wikipedia.py
+++ sparse_coding/tokenize_wikipedia.py
@@ -10,25 +10,20 @@ wikipedia_languages = ["en", "de"]
 datasets = {lang: [] for lang in wikipedia_languages}
 
 
-# def preprocess_data(text: str):
-#     '''Roughly remove the metadata lines, e.g. <Chapter 1> and <Speaker 1>'''
-#     return re.sub(r'<.*>', '', text)
-
-
 def add_to_dataset(text: str, language: str, min_chars=500):
     '''Get lines of sufficient length and add to the global datasets dictionary'''
     # data = preprocess_data(text)
     for line in tqdm(data):
         if len(line['text']) > min_chars:
             datasets[language].append(line['text'])
+
+    with open(f'data/wikipedia/{language}_samples.json', 'w', encoding='utf-8') as f:
+        json.dump(datasets[language], f)
+    print(f'{language}: {len(datasets[language])}')
     
 if __name__ == "__main__":
+    os.makedirs('data/wikipedia', exist_ok=True)
+
     for language in wikipedia_languages:
         data = load_dataset("wikipedia", f"20220301.{language}", split="train")
         add_to_dataset(data, language)
-    
-    os.makedirs('data/wikipedia', exist_ok=True)
-    for language in wikipedia_languages:
-        print(f'{language}: {len(datasets[language])}')
-        with open(f'data/wikipedia/{language}_samples.json', 'w', encoding='utf-8') as f:
-            json.dump(datasets[language], f)
