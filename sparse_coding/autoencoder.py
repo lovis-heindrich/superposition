ecb6fec462823754499e3fdd0f54ee1cc28a669f
Lovis Heindrich <33863801+lovis-heindrich@users.noreply.github.com>
2023-11-02 17:17:47 +0100

    fixed cyclic import issue

---
+++ sparse_coding/autoencoder.py
@@ -0,0 +1,42 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+
+class AutoEncoder(nn.Module):
+    def __init__(
+        self, d_hidden: int, l1_coeff: float, d_in: int, dtype=torch.float32, seed=47
+    ):
+        super().__init__()
+        torch.manual_seed(seed)
+        self.W_enc = nn.Parameter(
+            torch.nn.init.kaiming_uniform_(torch.empty(d_in, d_hidden, dtype=dtype))
+        )
+        self.W_dec = nn.Parameter(
+            torch.nn.init.kaiming_uniform_(torch.empty(d_hidden, d_in, dtype=dtype))
+        )
+        self.b_enc = nn.Parameter(torch.zeros(d_hidden, dtype=dtype))
+        self.b_dec = nn.Parameter(torch.zeros(d_in, dtype=dtype))
+
+        self.W_dec.data[:] = self.W_dec / self.W_dec.norm(dim=-1, keepdim=True)
+
+        self.d_hidden = d_hidden
+        self.l1_coeff = l1_coeff
+
+    def forward(self, x: torch.Tensor):
+        x_cent = x - self.b_dec
+        acts = F.relu(x_cent @ self.W_enc + self.b_enc)
+        x_reconstruct = acts @ self.W_dec + self.b_dec
+        l2_loss = (x_reconstruct - x).pow(2).sum(-1).mean(0)
+        l1_loss = self.l1_coeff * (acts.abs().sum())
+        loss = l2_loss + l1_loss
+        return loss, x_reconstruct, acts, l2_loss, l1_loss
+
+    @torch.no_grad()
+    def remove_parallel_component_of_grads(self):
+        W_dec_normed = self.W_dec / self.W_dec.norm(dim=-1, keepdim=True)
+        W_dec_grad_proj = (self.W_dec.grad * W_dec_normed).sum(
+            -1, keepdim=True
+        ) * W_dec_normed
+        self.W_dec.grad -= W_dec_grad_proj
+        self.W_dec.data = W_dec_normed
