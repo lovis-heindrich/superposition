913b9936d3fa39ab79a8335fb5807eeab12c56f9
Lucia Quirke <luciarosequirke@gmail.com>
2023-11-23 08:10:06 +0000

    fix reg bug, add model eval

--- sparse_coding/regularization.py
+++ sparse_coding/regularization.py
@@ -10,18 +10,18 @@ def regularization(reg_fn: Callable):
 @regularization
 def hoyer(acts: torch.Tensor, reg_coeff: float) -> torch.Tensor:
     '''hoyer square'''
-    l1 = acts.abs().sum()
-    l2 = (acts ** 2).sum().sqrt()
-    hoyer_square = l1 ** 2 / l2
+    l1_squared = acts.abs().sum() ** 2
+    l2_squared = (acts ** 2).sum()
+    hoyer_square = l1_squared  / l2_squared
     return hoyer_square * reg_coeff
 
 
 @regularization
 def hoyer_d(acts: torch.Tensor, reg_coeff: float) -> torch.Tensor:
     '''from paper on signSGD'''
-    l1 = acts.abs().sum()
-    l2 = (acts ** 2).sum().sqrt()
-    hoyer_square = l1 ** 2 / l2 
+    l1_squared = acts.abs().sum() ** 2
+    l2_squared = (acts ** 2).sum()
+    hoyer_square = l1_squared  / l2_squared
     hoyer_square_normalized = hoyer_square / acts.shape[-1]
     return hoyer_square_normalized * reg_coeff
 
