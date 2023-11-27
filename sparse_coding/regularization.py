100f5c0b40f14b97d525c3caff7647483195ffac
lovis-heindrich <heindrich.lovis@gmail.com>
2023-11-27 17:52:21 +0000

    hoyer sqrt combination

--- sparse_coding/regularization.py
+++ sparse_coding/regularization.py
@@ -38,6 +38,14 @@ def combined_hoyer_l1(acts: torch.Tensor, reg_coeffs: list[float]) -> list[torch
     return [l1_loss, hoyer_loss]
 
 @regularization
+def combined_hoyer_sqrt(acts: torch.Tensor, reg_coeffs: list[float]) -> list[torch.Tensor]:
+    l1_coeff = reg_coeffs[0]
+    hoyer_coeff = reg_coeffs[1]
+    l1_loss = sqrt(acts, l1_coeff)
+    hoyer_loss = hoyer_square(acts, hoyer_coeff)
+    return [l1_loss, hoyer_loss]
+
+@regularization
 def hoyer_d(acts: torch.Tensor, reg_coeff: float) -> torch.Tensor:
     '''from paper on signSGD'''
     l1_squared = acts.abs().sum() ** 2
