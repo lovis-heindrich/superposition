6c4f8a84082b569f2efd540a4823bd6eb3f8c40d
lovis-heindrich <heindrich.lovis@gmail.com>
2023-11-28 14:49:22 +0000

    l1 training run

--- sparse_coding/train_autoencoder.py
+++ sparse_coding/train_autoencoder.py
@@ -401,7 +401,7 @@ DEFAULT_CONFIG = {
     "cfg_file": None,
     "data_path": "/workspace/data/tinystories",
     "save_path": "/workspace",
-    "use_wandb": True,
+    "use_wandb": False,
     "num_eval_tokens": 800000,  # Tokens used to resample dead directions
     "num_training_tokens": 5e8,
     "batch_size": 4096,  # Batch shape is batch_size, d_in
@@ -413,13 +413,13 @@ DEFAULT_CONFIG = {
     "expansion_factor": 4,
     "seed": 47,
     "lr": 1e-4,
-    "l1_coeff": (0.0001, 0.00015),  # Used for all regularization types to maintain backwards compatibility
+    "l1_coeff": 0.0002, #(0.0001, 0.00015),  # Used for all regularization types to maintain backwards compatibility
     "wd": 1e-2,
     "beta1": 0.9,
     "beta2": 0.99,
     "num_eval_prompts": 200,  # Used for periodic evaluation logs
     "save_checkpoint_models": False,
-    "reg": "combined_hoyer_sqrt", # l1 | sqrt | hoyer | hoyer_d | hoyer_d_scaled_l1 | combined_hoyer_l1 | combined_hoyer_sqrt
+    "reg": "l1", # l1 | sqrt | hoyer | hoyer_d | hoyer_d_scaled_l1 | combined_hoyer_l1 | combined_hoyer_sqrt
     "finetune_encoder": None,
 }
 
