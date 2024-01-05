ab4f08921bb766387f0c00b6dc4a2924efcef4bf
Lucia Quirke <luciarosequirke@gmail.com>
2024-01-05 04:38:09 +0000

    plot sqrt reg and reorder reg methods

--- sparse_coding/regularization.py
+++ sparse_coding/regularization.py
@@ -7,18 +7,21 @@ def regularization(reg_fn: Callable):
     return reg_fn
 
 @regularization
-def hoyer_d_scaled_l1(acts: torch.Tensor, reg_coeff: float) -> torch.Tensor:
-    l1_squared = acts.abs().sum() ** 2
-    l2_squared = (acts ** 2).sum()
-    hoyer_square = l1_squared  / l2_squared
-    hoyer_square_normalized = hoyer_square / acts.shape[-1]
-    return hoyer_square_normalized * reg_coeff * acts.abs().sum()
+def l1(acts: torch.Tensor, scale: float) -> torch.Tensor:
+    if isinstance(scale, list) or isinstance(scale, tuple):
+        scale = scale[0]
+    return scale * acts.abs().sum()
 
 @regularization
-def l1(acts: torch.Tensor, reg_coeff: float) -> torch.Tensor:
-    if isinstance(reg_coeff, list) or isinstance(reg_coeff, tuple):
-        reg_coeff = reg_coeff[0]
-    return reg_coeff * acts.abs().sum()
+def sqrt(acts: torch.Tensor, scale: float | list[float]) -> torch.Tensor:
+    '''Square root penalty in [0, 1] and L1 elsewhere'''
+    if isinstance(scale, list) or isinstance(scale, tuple):
+        scale = scale[0]
+
+    l1 = acts.abs()
+    mask = l1 < 1
+    loss = (l1.sqrt() * mask) + (l1 * ~mask)
+    return loss.sum() * scale
 
 @regularization
 def hoyer_square(acts: torch.Tensor, scale: float, dim: int = -1) -> torch.Tensor:
@@ -34,39 +37,19 @@ def hoyer_square(acts: torch.Tensor, scale: float, dim: int = -1) -> torch.Tenso
 
 @regularization
 def combined_hoyer_l1(acts: torch.Tensor, reg_coeffs: list[float]) -> list[torch.Tensor]:
-    l1_coeff = reg_coeffs[0]
-    hoyer_coeff = reg_coeffs[1]
+    l1_coeff, hoyer_coeff = reg_coeffs[0], reg_coeffs[1]
+
     l1_loss = l1(acts, l1_coeff)
     hoyer_loss = hoyer_square(acts, hoyer_coeff)
     return [l1_loss, hoyer_loss]
 
 @regularization
 def combined_hoyer_sqrt(acts: torch.Tensor, reg_coeffs: list[float]) -> list[torch.Tensor]:
-    sqrt_coeff = reg_coeffs[0]
-    hoyer_coeff = reg_coeffs[1]
+    sqrt_coeff, hoyer_coeff = reg_coeffs[0], reg_coeffs[1]
+
     sqrt_loss = sqrt(acts, sqrt_coeff)
     hoyer_loss = hoyer_square(acts, hoyer_coeff)
     return [sqrt_loss, hoyer_loss]
-
-@regularization
-def hoyer_d(acts: torch.Tensor, reg_coeff: float) -> torch.Tensor:
-    '''from paper on signSGD'''
-    l1_squared = acts.abs().sum() ** 2
-    l2_squared = (acts ** 2).sum()
-    hoyer_square = l1_squared  / l2_squared
-    hoyer_square_normalized = hoyer_square / acts.shape[-1]
-    return hoyer_square_normalized * reg_coeff
-
-@regularization
-def sqrt(acts: torch.Tensor, scale: float | list[float]) -> torch.Tensor:
-    '''Square root penalty in [0, 1] and L1 elsewhere'''
-    if isinstance(scale, list) or isinstance(scale, tuple):
-        scale = scale[0]
-
-    l1 = acts.abs()
-    mask = l1 < 1
-    loss = (l1.sqrt() * mask) + (l1 * ~mask)
-    return loss.sum() * scale
 
 # Just for reference
 def hoyer_sqrt_reg(acts: torch.Tensor, hoyer_coeff: float, l1_coeff: float):
@@ -80,3 +63,20 @@ def hoyer_sqrt_reg(acts: torch.Tensor, hoyer_coeff: float, l1_coeff: float):
     l1_sqrt = act_reg_loss * ~mask 
     l1_sqrt += (act_reg_loss * mask) ** 0.5
     return hoyer_coeff * hoyer_squared + l1_coeff * l1_sqrt.sum()
+
+@regularization
+def hoyer_d_scaled_l1(acts: torch.Tensor, scale: float) -> torch.Tensor:
+    l1_squared = acts.abs().sum() ** 2
+    l2_squared = (acts ** 2).sum()
+    hoyer_square = l1_squared  / l2_squared
+    hoyer_square_normalized = hoyer_square / acts.shape[-1]
+    return hoyer_square_normalized * scale * acts.abs().sum()
+
+@regularization
+def hoyer_d(acts: torch.Tensor, scale: float) -> torch.Tensor:
+    '''from paper on signSGD'''
+    l1_squared = acts.abs().sum() ** 2
+    l2_squared = (acts ** 2).sum()
+    hoyer_square = l1_squared  / l2_squared
+    hoyer_square_normalized = hoyer_square / acts.shape[-1]
+    return hoyer_square_normalized * scale
